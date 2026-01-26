a219e46433f719ff92774165b7c5c093157cb0d4
minbyeongoh <minbyeongoh@gmail.com>
2026-01-26 23:38:19 +0900

    Title: 사칙연산 연습, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/사칙연산 연습/practice-basic-arithmetic-operations.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 5, b = 30, c;
+    c = a + b;
+    printf("%d\n", c/8);
+    printf("%d\n", c%8);
+    return 0;
+}
