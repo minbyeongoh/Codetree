23bb661c8748c06f0ed57fb826ae407815e0aa93
minbyeongoh <minbyeongoh@gmail.com>
2026-01-27 23:52:43 +0900

    Title: 합과 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/합과 평균/sum-and-avg.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d %.1lf", A+B, ((double)A+(double)B)/2);
+    return 0;
+}
