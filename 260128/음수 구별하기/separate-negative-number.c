e7244c7670352b06521631e35bb7dc1d99dd0af1
minbyeongoh <minbyeongoh@gmail.com>
2026-01-28 22:09:24 +0900

    Title: 음수 구별하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int N;
+    scanf("%d",&N);
+    printf("%d\n", N);
+    if(N<0) {
+        printf("minus");
+    }
+    return 0;
+}
