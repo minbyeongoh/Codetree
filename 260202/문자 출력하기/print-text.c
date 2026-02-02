8257f72994d94f3017c6c3b6b33ec95802d135a4
minbyeongoh <minbyeongoh@gmail.com>
2026-02-02 16:15:33 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260202/문자 출력하기/print-text.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int i;
+    char ch;
+    scanf("%c",&ch);
+    for(i=0; i<8; i++) {
+        printf("%c", ch);
+    }
+    return 0;
+}
