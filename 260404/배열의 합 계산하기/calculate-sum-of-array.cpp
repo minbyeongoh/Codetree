eff4222057af3f94299db14987f3948ca534f73c
minbyeongoh <minbyeongoh@gmail.com>
2026-04-04 14:22:06 +0900

    Title: 배열의 합 계산하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/배열의 합 계산하기/calculate-sum-of-array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int Array1[5] ={3,1,4,5,9};
+    cout<<Array1[1]+Array1[3]+Array1[4];
+    return 0;
+}
