1686496915755d9a9d8754b38bb9e3c34c8e70c1
minbyeongoh <minbyeongoh@gmail.com>
2026-04-05 12:13:32 +0900

    Title: 배수의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260405/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class doubleCount{
+protected:
+    int num;
+    vector<int> v;
+public:
+    doubleCount() {
+        for(int i = 0; i < 10; i++) {
+            cin >> num;
+            v.push_back(num);
+        }
+    }
+    virtual void multiply(int m) {}
+};
+
+class multiplyCount : public doubleCount {
+    int count;
+public:
+    virtual void multiply(int m) {
+        count=0;
+        for(int i = 0; i < 10; i++) {
+            if(v[i]%m==0) count++;
+        }
+        cout<<count<<' ';
+    }
+};
+
+int main() {
+    // Please write your code here.
+    doubleCount *p = new multiplyCount;
+    p->multiply(3);
+    p->multiply(5);
+    return 0;
+}
