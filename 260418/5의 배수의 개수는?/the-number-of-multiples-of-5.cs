bac73d421ada8c372ee068a41acc5108ca1b7d63
minbyeongoh <minbyeongoh@gmail.com>
2026-04-18 12:16:14 +0900

    Title: 5의 배수의 개수는?, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260418/5의 배수의 개수는?/the-number-of-multiples-of-5.cs
@@ -0,0 +1,25 @@
+using System;
+
+class CountFiveMul {
+    int arr[,] = new int[4,4], count = 0;
+    public void ReadFindArr() {
+        for(int i=0; i<4; i++) {
+            string[] tmp = Console.ReadLine().Split();
+            for(int j=0; j<4; j++) {
+                arr[i,j] = int.Parse(tmp[j]);
+                if(arr[i,j]%5==0) count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        // Please write your code here.
+        CountFiveMul go = new CountFiveMul();
+        go.ReadFindArr;
+    }
+}
